7bb3c781ea7c188eb934aa5ce8098c8a14a96b56
jayaraju-bura <92459601+jayaraju-bura@users.noreply.github.com>
2021-10-13 22:24:50 +0530

    Update url_shortner.py
    Added Doc strings

--- url_shortner.py
+++ url_shortner.py
@@ -1,3 +1,6 @@
+"""
+This script takes a long URL as input from the user through CLI and shortens it and writes to file
+"""
 import csv
 import logging
 import sys
@@ -5,11 +8,12 @@ import random
 
 MAIN_URL_PREFIX = "https://shorturl.com/"
 FILE_NAME = "database/tiny_urls.csv"
-BASE_KEY = "AEIOSUMA23"
+BASE_KEY = "ACEGJLUXY6789"
 TEST_URLS = ["https://github.com/",
              "https://geeksforgeeks.org/",
              "https://hackerrank.com/",
-             "https://twitter.com/"]
+             "https://twitter.com/",
+             "https://economictimes.indiatimes.com/markets/stocks/stock-watch/tata-power-shares-up-8-58-as-nifty-gains/articleshow/86984317.cms"]
 
 
 class UrlShortner:
@@ -18,12 +22,21 @@ class UrlShortner:
         self.short_keys = []
 
     def short_url(self):
-        tiny_url = ''.join(random.choice(BASE_KEY) for x in range(8))
+        """
+        This method shortens the long url using the BASE_KEY by trial and error
+        :return: returns the shortened URL
+        """
+        tiny_url = ''.join(random.choice(BASE_KEY) for x in range(0, len(BASE_KEY)))
         while tiny_url in self.short_keys:
-            tiny_url = ''.join(random.choice(BASE_KEY) for x in range(8))
+            tiny_url = ''.join(random.choice(BASE_KEY) for x in range(0, len(BASE_KEY)))
         return MAIN_URL_PREFIX + tiny_url
 
     def search_url(self):
+        """
+        This method searches whether shortened url is already existed or not for
+        requested long URL
+        :return: returns the status whether or not shortened URL is found in the file
+        """
         url_found = None
         try:
             with open(FILE_NAME, 'r') as file:
@@ -36,16 +49,28 @@ class UrlShortner:
                 self.short_keys = short_keys
                 return url_found
 
-        except  IOError as error:
+        except IOError as error:
             logging.debug("Short URL for the requested long url doesn't exist")
             return url_found
 
     def write_to_file(self, short_url=None):
+        """
+        This method writes the shortened URL to file in the form of key-value pair
+        key - long URL
+        value - shortened URL
+        :param short_url:
+        """
         with open(FILE_NAME, 'a') as file:
             csv_writer = csv.writer(file, delimiter=':')
             csv_writer.writerow([self.long_url, short_url])
 
     def run(self):
+        """
+        This is the base method of this class, looks out for shortened URL
+        is existing or not for requested long URL. if it is found returns it otherwise
+        calls short_utl() method to create a tiny url for the long URL
+        :return:
+        """
         tiny_url = self.search_url()
         if tiny_url is None:
             tiny_url = self.short_url()
@@ -54,6 +79,10 @@ class UrlShortner:
         return tiny_url
 
     def run_tests(self):
+        """
+        This method tests the functionality of the class by taking few websites as examples
+        :return:
+        """
         for test_url in TEST_URLS:
             self.long_url = test_url
             self.run()
